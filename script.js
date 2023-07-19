c9d1e8927c812dfd60f98d8bae7578ad569c5e01
Rahul Prasad Shaw <61372250+rjrahul08@users.noreply.github.com>
2023-07-19 13:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+  if (arr.length < 2) {
+        return -Infinity;
+      }
+
+      // Remove duplicates and sort the array in descending order
+      const uniqueSortedArr = Array.from(new Set(arr)).sort((a, b) => b - a);
+
+      // Return the second element if it exists, otherwise return -Infinity
+      return uniqueSortedArr.length >= 2 ? uniqueSortedArr[1] : -Infinity;
+    }
+
+    // Example usage:
+    const arr = [4, 2, 7, 5, 5, 1];
+    console.log(secondHighest(arr));
